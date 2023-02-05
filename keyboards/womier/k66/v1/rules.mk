276db542203a74706b7fe495794be802b4495f40
Frederik <5511687+fightforlife@users.noreply.github.com>
2023-02-05 19:09:26 +0100

    better stacksizes, all activated RGB Effects work

--- keyboards/womier/k66/v1/rules.mk
+++ keyboards/womier/k66/v1/rules.mk
@@ -1,6 +1,6 @@
 # project specific files
 SRC = rgb_matrix.c  #to control underglow LEDs via I2C
 
-USE_EXCEPTIONS_STACKSIZE = 0x137
-USE_PROCESS_STACKSIZE = 0x175
+USE_EXCEPTIONS_STACKSIZE = 0x135
+USE_PROCESS_STACKSIZE = 0x195
 TOP_SYMBOLS = yes
